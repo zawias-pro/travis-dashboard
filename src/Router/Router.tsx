e858173fc4b570dfff53591b459061be1221e48e
zawias <zawias@zawias.pro>
2019-06-20 00:12:34 +0200

    sections basic components

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Form = () => <h2>Home</h2>;
-
-const Dashboard = () => <h2>Dashboard</h2>;
+import { Form } from '../sections/Form';
+import { Dashboard } from '../sections/Dashboard';
 
 const Router = () => (
   <BrowserRouter>
