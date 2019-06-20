4da6c7bccf879fd93c5455e356f57f46acd8c1e0
Paweł Zawiślak <zawias@zawias.pro>
2019-06-20 16:28:35 +0200

    RepoStringParser (#1)
    * RepoStringParser
    
    * config file

---
+++ src/service/RepoStringParser/RepoStringParser.ts
@@ -0,0 +1,16 @@
+import { Repository } from '../../interface/Repository';
+
+class RepoStringParser {
+  public static parse(repoString: string): Repository[] {
+    const lines = repoString
+      .split(/\n/)
+      .map(line => line.trim())
+      .filter(line => line.length > 0);
+
+    return lines.map(line => ({
+      name: line,
+      slug: encodeURIComponent(line),
+    }));
+  }
+}
+export { RepoStringParser };
---
+++ src/service/RepoStringParser/index.ts
@@ -0,0 +1,1 @@
+export { RepoStringParser } from './RepoStringParser';
