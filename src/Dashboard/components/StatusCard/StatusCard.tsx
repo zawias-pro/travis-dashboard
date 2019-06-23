6c0bc286f5087c66ff78869290ba619a91fd8f55
zawias <zawias@zawias.pro>
2019-06-23 12:35:53 +0200

    grid instead of table

--- src/Dashboard/components/StatusCard/StatusCard.tsx
+++ src/Dashboard/components/StatusCard/StatusCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Chip, createStyles, makeStyles, Paper, Theme, Typography } from '@material-ui/core';
+import { Chip, createStyles, Grid, makeStyles, Paper, Theme, Typography } from '@material-ui/core';
 import {
   Message as MessageIcon,
   Person as PersonIcon,
@@ -43,34 +43,38 @@ const StatusCard: React.FunctionComponent<StatusProps> = ({
     return null;
   }
 
-  const { build, branch } = status.body;
+  const {build, branch} = status.body;
   const backgroundColor = getCardColor(status);
   const commitTitle = build.commit.split(/\n/)[0];
 
   return (
-    <Paper className={classes.paper} style={{ backgroundColor }}>
-      <Typography variant="h5">
-        {status.name} <Chip label={branch} />
-      </Typography>
-      <table className={classes.table}>
-        <tbody>
-          <tr>
-            <td colSpan={2} style={{ width: '100%' }}>
-              <MessageIcon className={classes.icon} /> {commitTitle}
-            </td>
-          </tr>
-          <tr>
-            <td>{build.previousState} -> {build.state}</td>
-            <td><PersonIcon className={classes.icon} /> {build.author}</td>
-          </tr>
+    <Paper className={classes.paper} style={{backgroundColor}}>
+      <div style={{display: 'flex', flexDirection: 'column', alignContent: 'spaceBetween', height: 150 }}>
+        <Typography variant="h5">
+          {status.name} <Chip label={branch}/>
+        </Typography>
+        <Grid container>
+          <Grid item xs={12}>
+            <MessageIcon className={classes.icon}/> {commitTitle}
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            {build.previousState} -> {build.state}
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PersonIcon className={classes.icon}/> {build.author}
+          </Grid>
           {build.finishedAt && (
-            <tr>
-              <td><CalendarIcon className={classes.icon} /> {build.finishedAt.format()}</td>
-              <td><TimeIcon className={classes.icon} /> {build.durationMinutes} min</td>
-            </tr>
+            <>
+              <Grid item xs={12} sm={6}>
+                <CalendarIcon className={classes.icon}/> {build.finishedAt.format()}
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TimeIcon className={classes.icon}/> {build.durationMinutes} min
+              </Grid>
+            </>
           )}
-        </tbody>
-      </table>
+        </Grid>
+      </div>
     </Paper>
   );
 };
